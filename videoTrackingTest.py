75062907ec9ee797e81747c364d5a1044797b9c8
Ryan Leska <45864749+rxleska@users.noreply.github.com>
2019-01-26 15:04:24 -0600

    Side Branch Of A Data Sending Test

--- videoTrackingTest.py
+++ videoTrackingTest.py
@@ -11,10 +11,26 @@
 import cv2
 import numpy as np
 import math
+import pickle
+import socket
+import sys
+import struct
 from matplotlib import pyplot as plt 
 
+print(f'Video Tracking DeepSpace:2606')
+
+editorMode = False # False when running on Rpi 
+sendContourProcessedImage  = False 
+sendImageFrameRate = 30
+
+Host = '127.0.0.1' # CHANGE THIS to roboRio Network Ip address
+Port = 5804
+
+sendSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Tcp Connection
+sendSocket.connect((Host, Port))
 
 cap = cv2.VideoCapture(0)
+
 # Configure Camera
 # Main Settings
 cap.set(cv2.CAP_PROP_EXPOSURE, -8) # -8 is Around 6 ms exposure aparently
@@ -39,39 +55,67 @@ cap.set(cv2.CAP_PROP_GAIN, 0.0)
 
 pictureCenter = (319.5,239.5)
 focalLength = 713.582
+exposureResetCounter = 0
+imageSendCounter = 0
+
+print('Camera has been configured: 640x480 30fps')
 
 while(cap.isOpened()):
+  
+  exposureResetCounter = exposureResetCounter + 1
+  if exposureResetCounter == 29:
+    exposureResetCounter = 0
+    cap.set(cv2.CAP_PROP_EXPOSURE, -8) # -8 is Around 6 ms exposure aparently
+    
   # Capture frame-by-frame
   ret, frame = cap.read()
   if ret == True:
 
     imgHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    imgGreenBW = cv2.inRange(imgHSV, np.array([30, 150, 80]), np.array([100, 255, 255]))
+    imgGreenBW = cv2.inRange(imgHSV, np.array([30, 150, 40]), np.array([100, 255, 255]))
 
 
     imgGreenBGR = cv2.cvtColor(imgGreenBW, cv2.COLOR_GRAY2BGR)
     img2, contours, hierarchy = cv2.findContours(imgGreenBW,  cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    minArea = 60
+
+    contoursSorted = sorted(contours,key=lambda x: cv2.contourArea(x), reverse = True)
 
     adjCoutours = []
-    for contour in contours:
-        contourArea = cv2.contourArea(contour)
-        #print (contourArea)
-        if contourArea > minArea:
-            adjCoutours.append(contour)
-
-    for contour in adjCoutours:
-        cv2.drawContours(imgGreenBGR, [contour], 0, (255, 0, 255), 3)
-
     points = []
-    for contour in adjCoutours:
-        moments = cv2.moments(contour)
-        centerX = int(moments['m10']/moments['m00'])
-        centerY = int(moments['m01']/moments['m00'])
-        pt = (centerX, centerY)
-        cv2.circle(imgGreenBGR, pt, 3, (255, 0, 0), 3)
-        points.append(pt)
+
+    if len(contoursSorted) >= 2:
+        adjCoutours.append(contoursSorted[0])
+        adjCoutours.append(contoursSorted[1])
+
+    # minArea = 60
+
+
+    # adjCoutours = []
+    # for contour in contours:
+    #     contourArea = cv2.contourArea(contour)
+    #     #print (contourArea)
+    #     if contourArea > minArea:
+    #         adjCoutours.append(contour)
+
+    
+
+
+        if editorMode == True or sendContourProcessedImage == True: 
+            for contour in adjCoutours:
+                cv2.drawContours(imgGreenBGR, [contour], 0, (255, 0, 255), 3)
+
+        for contour in adjCoutours:
+            moments = cv2.moments(contour)
+            moment0 = moments['m00']
+            if moment0 != 0: 
+                centerX = int(moments['m10']/moment0)
+                centerY = int(moments['m01']/moment0)
+                pt = (centerX, centerY)
+                if editorMode == True: 
+                    cv2.circle(imgGreenBGR, pt, 3, (255, 0, 0), 3)
+                points.append(pt)
+
 
     if len(points) == 2:
 
@@ -82,36 +126,57 @@ while(cap.isOpened()):
         center = (int(x), int(y))
 
         # print points, draw as 3 pixel wide 3 pixel thick circle in red 
-        print( f'left point at {pt1}' )
-        print( f'right point at {pt2}' )
-        print( f'center is {center}')
-        cv2.circle(imgGreenBGR, center, 3, (0, 0, 255), 3)
+        if editorMode == True:   
+            print( f'left point at {pt1}' )
+            print( f'right point at {pt2}' )
+            print( f'center is {center}')
+        
+        if editorMode == True or sendContourProcessedImage == True:
+            cv2.circle(imgGreenBGR, center, 3, (0, 0, 255), 3)
 
         #Angle to Target
         targetRadian = math.atan((center[0]-pictureCenter[0])/focalLength)
         targetAngle = math.degrees(targetRadian)
-        print( f'angle is {targetAngle}')
+        if editorMode == True: 
+            print( f'angle is {targetAngle}')
 
+        message = struct.pack('!idd', 1, targetAngle, 0.0)
+        sendSocket.send(message)
 
+    else:
+        message = struct.pack('!i', 2)
+        sendSocket.send(message)
 
-    # Display the resulting frame
-    cv2.imshow('Frame', imgGreenBGR)
-    #Frame without centerpoints cv2.imshow('Frame2',imgGreenBW )
-    cv2.imshow('display',frame)
-    #cv2.imshow('displayConvtoHSV',imgHSV)
+    if sendContourProcessedImage == True:
+        imageSendCounter = imageSendCounter + 1
+        if imageSendCounter >= sendImageFrameRate:
+            imageSendCounter = 0
+            result, data = cv2.imencode('.jpg ', imgGreenBGR)
+            contourProcessedImageData = pickle.dumps(data, False)
+            contourProcessedImageDataSize = len(contourProcessedImageData)
+            message = struct.pack('!ii', 3, contourProcessedImageDataSize)
+            print(f'Send image Size={contourProcessedImageDataSize}')
+            sendSocket.send(message)
+            sendSocket.sendall(contourProcessedImageData)
 
-    #TODO REMEMBER TO REMOVE KEY PRESS AND SEND NETWORKTABLES HERE
+    if editorMode == True:    
+        # Display the resulting frame
+        cv2.imshow('Frame', imgGreenBGR)
+        #Frame without centerpoints cv2.imshow('Frame2',imgGreenBW )
+        cv2.imshow('display',frame)
+        #cv2.imshow('displayConvtoHSV',imgHSV)
 
-    # Press Q on keyboard to  exit
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-      break
+        
+        # Press Q on keyboard to  exit
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
  
   # Break the loop
   else: 
     break
-
-# Wait for a key press and close all windows
-cv2.waitKey(0)
-cv2.destroyAllWindows() 
+if editorMode == True:
+    # Wait for a key press and close all windows
+    cv2.waitKey(0)
+    cv2.destroyAllWindows() 
 
 
